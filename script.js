4672f94c2bfed2dcf707bf9cbca5006361baa90c
annadowell <146206333+annadowell@users.noreply.github.com>
2024-03-14 15:35:32 +0000

    mapbox prototype

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
-// let map = L.map('map').setView([54.805, -2.799], 6);
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
+
+let map = L.map('map').setView([-4.880576, 54.336061], 4);
 
 
-// Define the Stadia Stamen Terrain tile layer with an API key.
-// This creates a new tile layer using Stadia Maps' Stamen Terrain style. 
-// The URL includes placeholders for coordinates ({z}, {x}, {y}) and a parameter for the API key.
-var Stadia_StamenTerrainBackground = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png?api_key=326d8a1b-041f-44ad-aa58-fdfdce54c9ca', {
-    minZoom: 0,    // The minimum zoom level at which this layer will be visible.
-    maxZoom: 18,   // The maximum zoom level at which this layer will be visible.
-    attribution: '...' // Attribution text displayed on the map, as required by the data providers.
-});
-
-// Create the map object and set its initial view.
-// 'L.map' creates a new map instance, and 'setView' sets the initial geographic center and zoom level.
-let map = L.map('map').setView([52.705, -2.099], 6.5);
-
-// Add the Stadia Stamen Terrain layer to the map.
-// This makes the Stadia Stamen Terrain layer visible on the map.
-Stadia_StamenTerrainBackground.addTo(map);
 
 // Initialize objects to store data about stations and rainfall.
 let stationData = {};
 let rainfallData = {};
 
-// Function to fetch rainfall data for all stations.
-// This function makes a network request to an API, processes the JSON response,
-// and stores rainfall data keyed by station codes in 'rainfallData'.
+// Function to fetch rainfall data for all stations and store data keyed by station codes in 'rainfallData'.
 function fetchAllRainfallData() {
     return fetch('https://environment.data.gov.uk/flood-monitoring/id/measures?parameter=rainfall')
         .then(response => response.json())
@@ -56,9 +35,7 @@ function fetchAllRainfallData() {
         .catch(error => console.error("FETCH ERROR:", error));
 }
 
-
-// Function to fetch the latest locations of the UK Environment Agency stations and update our map.
-// This function retrieves station data and then calls 'updateStationsOnMap' to add markers to the map.
+// Function to retrieve station data and then call 'updateStationsOnMap' to add markers to the map.
 function getUKStations() {
     return fetch('https://environment.data.gov.uk/flood-monitoring/id/stations?parameter=rainfall')
         .then(response => response.json())
@@ -71,8 +48,6 @@ function getUKStations() {
 }
 
 // Function to determine the circle radius based on rainfall amount.
-// This function calculates the radius of a circle marker based on the amount of rainfall,
-// using a base radius for zero rainfall and increasing the radius for higher rainfall amounts.
 function getCircleRadius(rainfallAmount) {
     const baseRadius = 1150; // Base radius for circles representing 0 mm rainfall
     if (rainfallAmount > 0) {
@@ -107,22 +82,6 @@ function createSvgGradient(id, innerColor, outerColor) {
     return 'url(#' + id + ')';
 }
 
-// function displayTotalRainfall(totalRainfall) {
-//     var info = L.control();
-
-//     info.onAdd = function (map) {
-//         this._div = L.DomUtil.create('div', 'info'); // Create a div with a class "info"
-//         this.update();
-//         return this._div;
-//     };
-
-//     // Method to update the control based on feature properties passed
-//     info.update = function () {
-//         this._div.innerHTML = '<h4>Total Rainfall</h4>' + totalRainfall.toFixed(2) + ' mm';
-//     };
-
-//     info.addTo(map);
-// }
 
 function displayTotalRainfall(totalRainfall) {
     console.log("Total Rainfall: ", totalRainfall); // Log the total rainfall
@@ -141,36 +100,16 @@ function updateStationsOnMap(stations, color, limit) {
                 let rainfallAmount = rainfallData.hasOwnProperty(stationCode) ? rainfallData[stationCode] : 0;
                 let circleRadius = getCircleRadius(rainfallAmount);
                 
-                // Adjust the circle style based on the rainfall amount
-                let circleStyle = {};
-                if (rainfallAmount > 0) {
-                    // For non-zero rainfall, use a gradient
-                    circleStyle = {
-                        color: 'transparent', // This removes the bluish edge
-                        fillColor: getGradientFillColor(color),
-                        fillOpacity: 0.9,
-                        weight: 0,
-                        radius: circleRadius,
-                    };
-                } else {
-                    // For zero rainfall, use a simple fill
-                    circleStyle = {
-                        color: color,
-                        fillColor: color,
-                        fillOpacity: 0.4,
-                        weight: 0,
-                        radius: circleRadius,
-                    };
-                }
+                var el = document.createElement('div');
+                el.className = 'marker';
+                el.style.background = color;
+                el.style.width = circleRadius * 2 + 'px';
+                el.style.height = circleRadius * 2 + 'px';
 
-                // Create a circle marker with the specified style
-                let circle = L.circle([stationLat, stationLng], circleStyle);
-
-                let popupContent = "Station: " + stationCode + "<br>Rainfall: " + rainfallAmount + " mm";
-                circle.bindPopup(popupContent);
-
-                circle.addTo(map);
-                stationData[stationCode] = { marker: circle };
+                new mapboxgl.Marker(el)
+                    .setLngLat([stationLng, stationLat])
+                    .setPopup(new mapboxgl.Popup().setHTML("Station: " + stationCode + "<br>Rainfall: " + rainfallAmount + " mm"))
+                    .addTo(map);
             }
         }
     });
@@ -183,22 +122,6 @@ function getGradientFillColor(baseColor) {
     return gradientUrl;
 }
 
-// function displayStationsWithRain(stationsWithRain) {
-//     var info = L.control({position: 'bottomright'}); // Position the control at the bottom right
-
-//     info.onAdd = function (map) {
-//         this._div = L.DomUtil.create('div', 'info'); // Create a div with a class "info"
-//         this.update();
-//         return this._div;
-//     };
-
-//     // Update the control based on feature properties passed
-//     info.update = function () {
-//         this._div.innerHTML = '<h4>Stations with Rain</h4>' + stationsWithRain + ' stations';
-//     };
-
-//     info.addTo(map);
-// }
 
 function displayStationsWithRain(stationsWithRain) {
     console.log("Stations with Rain: ", stationsWithRain); // Log the number of stations
@@ -206,11 +129,6 @@ function displayStationsWithRain(stationsWithRain) {
 }
 
 
-// The rest of the createSvgGradient function remains the same
-
-// Sequentially fetch rainfall data and then station details.
-// First, it fetches all rainfall data, and once that's complete,
-// it proceeds to fetch and process station data.
 fetchAllRainfallData().then(() => {
     getUKStations();
 });
